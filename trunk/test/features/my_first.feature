e8af007526e246209a901f6d79ed57e8b6641770
barna.fulop <barna.fulop@accenture.com>
2014-12-16 14:18:32 +0200

    added calabash

---
+++ trunk/test/features/my_first.feature
@@ -0,0 +1,11 @@
+Feature: Running a test
+  As an iOS developer
+  I want to have a sample feature file
+  So I can begin testing quickly
+
+
+ Scenario: As a valid user I can log into my app
+    When I press "Login"
+    Then I see "Welcome to coolest app ever"
+
+
--- trunk/test/test/ViewController.m
+++ trunk/test/test/ViewController.m
@@ -9,21 +9,38 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    IBOutlet UILabel *label;
+}
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
+- (void)viewDidLoad {
+	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
 }
 
+- (void)somerRandomFunction {
+	if (1 == 1) {
+		for (;; ) {
+			//comment
+		}
+	}
+	else if (2 == 2) {
+	}
+	NSString *string = @"asldk asdkladk asd ";
+	NSString *stringTestString = @"asldk asdkladk asd ";
+	NSString *stringTestStringstringTestString = @"asldk asdkladk asd";
+}
+
+- (IBAction)LoginTapped:(id)sender {
+    
+    label.hidden = NO;
+}
 @end
